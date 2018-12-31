2e842ae77fb6f8f126624ad066048c64c3489762
marzzy mogh <marzzy.m@gmail.com>
2018-12-31 17:53:47 +0330

    form am o dorost kardam balakhare

--- src/js/components/container/FormContainer.js
+++ src/js/components/container/FormContainer.js
@@ -1,6 +1,8 @@
 /* eslint-disable */
+
+"use strict";
 import React, { Component } from "react";
-import Input from "../presentational/Input";
+import {Input, Textarea} from "../presentational/Input";
 
 
 class FormContainer extends Component {
@@ -8,49 +10,98 @@ class FormContainer extends Component {
     super();
 
     this.state = {
-      name: ["ggff",false],
-      email:"",
-      txtbody: "",
+      name: { val: "", status: "", sFlag: ""},
+      email: { val: "", status: "", sFlag: "" },
+      txtbody: { val: "", status: "", sFlag: "" },
     };
 
-    this.handleChangeS = this.handleChangeSimple.bind(this);
-    this.handleChangeE = this.handleChangeEmail.bind(this);
+    this.handleChange = this.handleChangeFunc.bind(this);
   }
 
-  handleChangeSimple(event) {
-    // (console.log(event.target.value.length));
-    (console.log(event.target.value));
-    this.setState({ [event.target.id[0]]: event.target.value }) 
-    (event.target.value.length)?
-    console.log('omad'):console.log('naUmad');
-
+  // tracking changes in form and answer them
+  handleChangeFunc(event) {
+    console.log([event.target.id]);
+    this.setState({ [event.target.id]: { val: event.target.value, status: "", sFlag: "" } });
+    this.handleStatus(event.target.id, event.target.value);
   }
-  handleChangeEmail(event) {
-    // console.log();
-    // (event.target.value)?
-      this.setState({ [event.target.id]: event.target.value })
+
+  // set status relay on input values 
+  handleStatus(attrName, attrVal) {
+    let [right, wrong, warning] = ['fa-right', 'fa-wrong', 'fa-warning'];
+    let errStatus, flgStatus;
+
+    if (attrName == 'name') {
+      if (!attrVal.length) {
+        errStatus = "لطفا فیلد را پر نمایید." ;
+        flgStatus = wrong;
+      } else if (attrVal.length <= 2) {
+        errStatus = "اسم کمتر از ۳ حرف وجود ندارد. لطفا نام صحیح خود را وارد فرمایید." ;
+        flgStatus = warning;
+      } else {
+        errStatus = "اسم وارد شده درست است." ;
+        flgStatus = right;
+      }
+    } else if (attrName == 'email') {
+      let firstATSignIndex = (Array.from(attrVal).indexOf('@'));
+      let lastDOTIndex = (Array.from(attrVal).lastIndexOf('.'));
+
+      if (!attrVal.length) {
+          errStatus = "لطفا فیلد را پر نمایید.";
+          flgStatus = wrong;
+      } else if (firstATSignIndex == -1){
+          errStatus = "ایمیل باید دارای @ باشد." ;
+          flgStatus = warning;
+      } else if (lastDOTIndex == -1) {
+          errStatus = "ایمیل باید دارای . باشد." ;
+          flgStatus = warning;
+      } else if (lastDOTIndex < firstATSignIndex) {
+          errStatus = "@ باید قبل . باشد." ;
+          flgStatus = warning;
+      } else if (lastDOTIndex == attrVal.length - 1) {
+          errStatus = "علامت . نمیتواند پایان ایمیل باشد." ;
+          flgStatus = warning;
+      } else {
+          errStatus = "ایمیل وارد شده درست است.";
+          flgStatus = right;
+      }
+    }
+    this.setState({ [attrName]: { val: attrVal, status: errStatus, sFlag: flgStatus } });
   }
 
   render() {
-    const { name: [nameval,namecheck], email } = this.state;
+    const { name, email, txtbody } = this.state;
     return (
-      <form id="article-form">
+      <form id="article-form" onSubmit={this.submitme}>
         <Input
           text="نام"
           label="name"
           type="text"
           id="name"
-          value={nameval}
-          handleChange={this.handleChangeS}
+          value={name.val}
+          handleChange={this.handleChange}
+          status={name.status} 
+          flag={name.sFlag}
         />
         <Input
           text="ایمیل"
           label="email"
           type="email"
           id="email"
-          value={email}
-          handleChange={this.handleChangeE}
+          value={email.val}
+          handleChange={this.handleChange}
+          status={email.status} 
+          flag={email.sFlag}
         />
+        <Textarea
+          // label, text, id, value, handleChange, status, flag
+          text="نظر خود را وارد فرمایید..."
+          label="نظر"
+          id="txtbody"
+          value={txtbody.val}
+          handleChange={this.handleChange}
+          status={txtbody.status}
+          flag={txtbody.sFlag}
+       />
       </form>
     );
   }
--- src/js/components/presentational/Input.js
+++ src/js/components/presentational/Input.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Input = ({ label, text, type, id, value, handleChange }) => (
-  <div className='form-group'>
-    <label htmlFor={label}>{text}</label>
-    <input
-      type={type}
-      className='form-control'
-      id={id}
-      value={value}
-      onChange={handleChange}
-      required
-    />
-  </div>
+const Input = ({ label, text, type, id, value, handleChange, status, flag }) => (
+  <>
+    <label htmlFor={label}>
+      {text}
+      <input
+        type={type}
+        className='form-control'
+        id={id}
+        value={value}
+        onChange={handleChange}
+        required
+      />
+    </label>
+    <span className={`${flag} status`} >{status}</span>
+  </>  
 )
 
 Input.propTypes = {
@@ -21,7 +24,35 @@ Input.propTypes = {
   type: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  flag: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired
 }
 
-export default Input
+const Textarea = ({ label, text, id, value, handleChange, status, flag }) => (
+  <>
+  <label htmlFor={label}>
+    {text}
+    <textarea
+      id={id}
+      value={value} 
+      onChange={handleChange}
+      required
+    />
+  </label>
+  <span className={`${flag} status`} >{status}</span>
+  </>
+)
+
+Textarea.propTypes = {
+  label: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  flag: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired
+}
+
+export { Input, Textarea };
+
